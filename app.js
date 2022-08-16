c014f4bcdd29aa0ff26ff77acda49253bd046975
Martinica Mare <mmare@azcuy.local>
2022-08-16 17:39:54 -0300

    Cambios

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ app.listen(3030, () => {
     console.log("Servidor funcionando");
 })
 
+const publicPath = .path.resolve (__dirname, "./public"); 
+app.use(express.static(publicPath));
+
+app.get(express.static(path.join(__dirname, "/public")))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+});
+
