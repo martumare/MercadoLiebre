6224b55359443ee6e668df2515b450615c278720
martumare <maremartinica@gmail.com>
2022-08-16 22:30:51 -0300

    Primer desafio

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ app.listen(3030, () => {
 const publicPath = path.resolve (__dirname, "./public"); 
 app.use(express.static(publicPath));
 
-app.get(express.static(path.join(__dirname, "/public")))
-
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
