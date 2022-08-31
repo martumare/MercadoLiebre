b9e572d013b935b83581400c3fccee95a37f3c2d
martumare <maremartinica@gmail.com>
2022-08-31 19:02:00 -0300

    Heroku

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const app = express();
 const path = require("path");
 
 
-/*app.listen(process.env.PORT || 3000, function() {
-    console.log("Servidor funcionando en el puerto 3030")
-});
-*/
-app.listen(3030, () => {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor funcionando en el puerto 3030")
 });
 
